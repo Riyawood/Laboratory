571011e0e3a79654b2d87e498ac76fefb8d76565
Riya <120563852+Riyawood@users.noreply.github.com>
2022-12-15 14:47:33 +0530

    Create Hello.java

---
+++ Assignment 15-12-22/Hello.java
@@ -0,0 +1,18 @@
+package com.java.demo;
+
+public class Hello {
+
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i;
+      for(i=0; i<5; i++)
+      {
+       System.out.println("Hello");
+      }
+      System.out.println();
+      for(i=5; i>0; i--)
+      {
+    	  System.out.println("World");
+      }
+   }  
+}
