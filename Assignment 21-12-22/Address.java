da77bbaa9f2e8a7c6d57524f86f1448a9064c32c
Riya <120563852+Riyawood@users.noreply.github.com>
2023-01-02 12:20:57 +0530

    Create Address.java

---
+++ Assignment 21-12-22/Address.java
@@ -0,0 +1,53 @@
+package oops;
+public class Address 
+{
+   int pincode;
+   String city, state, country;
+  
+	public Address(int pincode, String city, String state, String country) {
+	super();
+	this.pincode = pincode;
+	this.city = city;
+	this.state = state;
+	this.country = country;
+}
+	//getter setter method from source
+	public int getPincode()
+	{
+		return pincode;
+	}
+
+	public void setPincode(int pincode) 
+	{
+		this.pincode = pincode;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() 
+	{
+		return state;
+	}
+
+	public void setState(String state) 
+	{
+		this.state = state;
+	}
+
+	public String getCountry()
+	{
+		return country;
+	}
+	
+	public void setCountry(String country) 
+	{
+		this.country = country;
+	}
+}
