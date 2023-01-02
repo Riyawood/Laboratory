3a058139da06ba7e909e949e46827b8fe224ee00
Riya <120563852+Riyawood@users.noreply.github.com>
2023-01-02 12:15:44 +0530

    Create ReverseString.java

---
+++ Assignment 22/12/22/ReverseString.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class ReverseString 
+{
+	public static void main(String[] args) 
+	{
+		String a = "RIYA";
+		
+		for (int i= (a.length()-1); i>=0;i--) 
+		{
+			System.out.print(a.charAt(i));
+		}	
+	}
+}
