883c487e1de6d52e4040b4362772b5acea2c8482
Riya <120563852+Riyawood@users.noreply.github.com>
2023-01-30 14:47:38 +0530

    Create  MyThread.java

---
+++ Assignment 27-01-23/MyThread.java
@@ -0,0 +1,15 @@
+public class MyThread implements Runnable
+{
+	@Override
+	public void run()
+	{
+		System.out.println(Thread.currentThread().getName()+" is running...");
+	}
+
+	public static void main(String[] args)
+	{
+		MyThread mt = new MyThread();
+		Thread t1 = new Thread(mt, "Java Thread");  //new state
+		t1.start();
+	}
+}
