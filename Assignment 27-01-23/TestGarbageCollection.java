d6ca7422b46b98a479e833788b1410e22c06027a
Riya <120563852+Riyawood@users.noreply.github.com>
2023-01-30 14:48:20 +0530

    Create TestGarbageCollection.java

---
+++ Assignment 27-01-23/TestGarbageCollection.java
@@ -0,0 +1,16 @@
+public class TestGarbageCollection
+{
+	public void finalize()
+	{
+		System.out.println("All objects are collected...");
+	}
+	public static void main(String[] args)
+	{
+		TestGarbageCollection t1 = new TestGarbageCollection();
+		TestGarbageCollection t2 = new TestGarbageCollection();
+		t1= null; //setting t1 to be null or null referencing
+		t2= null;
+		System.gc();
+		System.out.println("All are unreferenced...");
+	}
+}
